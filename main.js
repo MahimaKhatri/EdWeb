ca06d5a91f020c46a5b61d294c086c544e36136f
Mahima Khatri <mahima8301@gmail.com>
2022-01-21 18:44:00 +0530

    Login Update

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
         $('.navbar').toggleClass('nav-toggle');
     });
 
+    $('#login').click(function(){
+        $('.login-form').addClass('popup');
+
+    });
+    $('.login-form form .fa-times').click(function(){
+        $('.login-form').removeClass('popup');
+
+    });
+
     $(window).on('load scroll', function () {
         $('#menu').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
